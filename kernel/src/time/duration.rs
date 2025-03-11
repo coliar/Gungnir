780ef21b8c9f0b3d7840c035c253b6739b243962
coliar <1664023607@qq.com>
2025-03-11 22:51:56 +0800

    tmp save 0312

--- kernel/src/time/duration.rs
+++ kernel/src/time/duration.rs
@@ -1,3 +1,5 @@
+use super::{GCD_1K, GCD_1M, TICK_HZ};
+
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) struct Duration {
     pub(super) ticks: u64,
@@ -11,5 +13,75 @@ impl Duration {
         self.ticks
     }
 
-    
+    pub(crate) const fn as_secs(&self) -> u64 {
+        self.ticks / TICK_HZ
+    }
+
+    pub(crate) const fn as_millis(&self) -> u64 {
+        self.ticks * (1_000 / GCD_1K) / (TICK_HZ / GCD_1K)
+    }
+
+    pub(crate) const fn as_micros(&self) -> u64 {
+        self.ticks * (1_000_000 / GCD_1M) / (TICK_HZ / GCD_1M)
+    }
+
+    pub(crate) const fn from_ticks(ticks: u64) -> Self {
+        Duration { ticks }
+    }
+
+    pub(crate) const fn from_secs(secs: u64) -> Self {
+        Duration { ticks: secs * TICK_HZ }
+    }
+
+    pub(crate) const fn from_millis(millis: u64) -> Self {
+        Duration {
+            ticks: div_ceil(millis * (TICK_HZ / GCD_1K), 1000 / GCD_1K),
+        }
+    }
+
+    pub(crate) const fn from_micros(micros: u64) -> Self {
+        Duration {
+            ticks: div_ceil(micros * (TICK_HZ / GCD_1M), 1_000_000 / GCD_1M),
+        }
+    }
+
+    pub(crate) const fn from_nanos(micros: u64) -> Self {
+        Duration {
+            ticks: div_ceil(micros * (TICK_HZ / GCD_1G), 1_000_000_000 / GCD_1G),
+        }
+    }
+
+    pub(crate) const fn from_secs_floor(secs: u64) -> Self {
+        Duration { ticks: secs * TICK_HZ }
+    }
+
+    pub(crate) const fn from_millis_floor(millis: u64) -> Self {
+        Duration {
+            ticks: millis * (TICK_HZ / GCD_1K) / (1_000 / GCD_1K),
+        }
+    }
+
+    pub(crate) const fn from_micros_floor(micros: u64) -> Self {
+        Duration {
+            ticks: micros * (TICK_HZ / GCD_1M) / (1_000_000 / GCD_1M),
+        }
+    }
+
+    pub(crate) const fn from_hz(hz: u64) -> Self {
+        let ticks = if hz >= TICK_HZ {
+            1
+        } else {
+            (TICK_HZ + hz / 2) / hz
+        };
+        Duration { ticks }
+    }
+
+    pub(crate) fn checked_add(&self, rhs: Duration) -> Option<Duration> {
+        self.ticks.checked_add(rhs.ticks).map(|ticks| Duration { ticks })
+    }
+}
+
+#[inline]
+const fn div_ceil(num: u64, den: u64) -> u64 {
+    (num + den - 1) / den
 }
